e4cf39943ce7f2ec5ea5bda36b1c9e06de040ee1
Li Yan <li.yan.ly414@gmail.com>
2017-03-02 18:01:38 -0500

    Run 20 demands on Rivanna

---
+++ cluster-20demands/test_bm_batch1_5demands2.m
@@ -0,0 +1,28 @@
+% simulation benchmark for Raymond, 5 demands, 20 simulations
+addpath(genpath('/scratch/ly6j/YALMIP')); 
+parpool(10); 
+
+%% Generate traffic matrix
+freqMax = 800;
+
+%% Run optimization - 1. preprocessing
+nDemands = 5;
+
+for j = 1 : 1
+    demandName = sprintf('demands_14nodes_matlab_%d.mat', j);
+    load(demandName);
+    demandPair = m(1:nDemands, :);
+    demandPairMatrix = demandPairMatrix(1:nDemands, :);
+    clear m;
+    tic;
+    result(j) = fcn_bm(freqMax, demandPair, demandPairMatrix, 15, 1);
+    runtime = toc;
+    fprintf('\nTraffic %d is finished using %.2f seconds.\n', j, runtime);
+    if ~exist('results', 'dir')
+        mkdir('results')
+    end
+    resultName = sprintf('benchmark_N%d_%d.mat', nDemands, j);
+    save(resultName)
+end
+
+exit;
---
+++ cluster-20demands/test_yalmip.m
@@ -0,0 +1,1 @@
+addpath(genpath('/scratch/ly6j/YALMIP'));
